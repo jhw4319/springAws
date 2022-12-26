6298aab4972ce0efcff97ab74a63e0f323cd0024
jhw4319 <jhw4319@naver.com>
2022-12-26 15:48:57 +0900

    Added Google login

---
+++ src/main/java/com/jhw/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.jhw.springboot.config.auth;
+
+import com.jhw.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // 스프링 시큐리티 설정 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable()
+                .and().authorizeRequests()
+                .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll() // 권한 관리 대상을 지정
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/")
+                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService); // 소셜 로그인 성공시 후속조치
+
+    }
+}
